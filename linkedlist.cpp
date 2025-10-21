9c9102488cc05aaf062db3311f21d232c594a6c8
quispedavid987 <quispedavid987@gmail.com>
2025-10-20 20:46:59 -0500

    LinkedList implementado

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -5,13 +5,22 @@ void DemoLinkedList(){
     CLinkedList<int> l1;
     int x = 5;
     l1.Insert(x, 3);
-    x = 7;
+    x = 9;
     l1.Insert(x, 14);
-    // std::cout << x;
-    // std::cout << l1;
-
+    std::cout << x;
+    std::cout << l1;
     CLinkedList<float> l2;
     float f = 3.14;
     l2.Insert(f, 10.5);
-    // std::cout << l2;
+    std::cout << l2;
+    l2.Insert(f, 21.0);
+    std::cout << l2;
+
+    CLinkedList<int> l3 = l1;
+    std::cout << l3;
+
+    CLinkedList<int> l4 = std::move(l1);
+    std::cout << l4;
+
+
 }
--- linkedlist.h
+++ linkedlist.h
@@ -1,48 +1,61 @@
 #ifndef __LINKEDLIST_H__
 #define __LINKEDLIST_H__
 #include "types.h"
+#include<utility>
+#include<iostream> //**LIBRERIAS ADICIONALES
 
+
+template <typename T>
+class CLinkedList; //****CORRECCION
 template <typename T>
 class LLNode{
 private:
     using    Type = T;
-    using    Node = typename LLNode<T>;
+    friend   class CLinkedList<T>;
     Type     m_data;
     Ref      m_ref;
-    Node    *m_pNext = nullptr;
+    LLNode<T>    *m_pNext = nullptr;
 
 public:
     LLNode(Type &elem, Ref ref, LLNode<T> *pNext = nullptr)
-        : m_data(elem), m_pNext(pNext){
+        : m_data(elem), m_ref(ref), m_pNext(pNext){ //PARAMETRO REF
     }
     Type   GetData()    { return m_data;     }
+    Ref    GetRef()     { return m_ref;     }
     Type  &GetDataRef() { return m_data;     }
-    Node * GetNext()    { return m_pNext;    }
-    Node *&GetNextRef() { return m_pNext;    }
+    LLNode<T> * GetNext()    { return m_pNext;    } //DE NODE A LLNODE<T>
+    LLNode<T> *&GetNextRef() { return m_pNext;    }
 };
 
 template <typename T>
 class CLinkedList{
 private:
     using Type = T; 
-    using Node = typename LLNode<Type>  ; 
-    // Node<Type> *m_pHead = nullptr;
+    using Node = LLNode<Type>  ; //typename NO NECESARIO
+    Node *m_pHead = nullptr;
 public:
     // Constructor
     CLinkedList();
     // TODO: Constructor Copia
     CLinkedList(CLinkedList &other);
+    CLinkedList& operator = (CLinkedList &other);
 
     // TODO: Move contructor
     CLinkedList(CLinkedList &&other);
+    CLinkedList& operator = (CLinkedList &&other);
 
     // Destructor seguro
     virtual ~CLinkedList();
 
     void Insert(Type &elem, Ref ref);
+
+    template<typename P> //INSERCION
+    friend std::ostream& operator<<(std::ostream& os, CLinkedList<P>& list);
 private:
     // TODO: Implementar
     void InternalInsert(Node *&rParent, Type &elem, Ref ref);
+    void Clear();
+    void CopyFrom(CLinkedList &other);
 };
 
 template <typename T>
@@ -61,23 +74,96 @@ void CLinkedList<T>::InternalInsert(Node *&rParent, Type &elem, Ref ref){
 }
 
 template <typename T>
-CLinkedList<T>::CLinkedList()
+CLinkedList<T>::CLinkedList() : m_pHead(nullptr)
 {
 }
 
+//IMPLEMENT CONST COPIA
 template <typename T>
-CLinkedList<T>::CLinkedList(CLinkedList &other)
+CLinkedList<T>::CLinkedList(CLinkedList &other) : m_pHead(nullptr)
 {
+    CopyFrom(other);
 }
 
+//IMPLEMENT MOVE CONST
 template <typename T>
 CLinkedList<T>::CLinkedList(CLinkedList &&other)
+    : m_pHead(other.m_pHead)
 {
+    other.m_pHead = nullptr;
 }
+
+//ASIG POR COPIA
+template <typename T>
+CLinkedList<T>& CLinkedList<T>::operator=(CLinkedList &other){
+    if (this != &other){
+        Clear();
+        CopyFrom(other);
+    }
+    return *this;
+}
+
+//OPERADOR ASIG POR MOV
+template <typename T>
+CLinkedList<T>& CLinkedList<T>::operator=(CLinkedList &&other) {
+    if (this != &other){
+        Clear();
+        m_pHead = other.m_pHead;
+        other.m_pHead = nullptr;
+    }
+    return *this;
+}
+
+//LIMPIAR LISTA
+template <typename T> 
+void CLinkedList<T>::Clear(){
+    Node *current = m_pHead;
+    while (current){
+        Node *next = current->m_pNext;
+        delete current;
+        current = next;
+    }
+    m_pHead = nullptr;
+}
+
+//COPIAR DESDE OTRA LISTA
+template <typename T>
+void CLinkedList<T>::CopyFrom(CLinkedList &other) {
+    if (!other.m_pHead){
+        m_pHead = nullptr;
+        return;
+    }
+
+    Node *otherCurent = other.m_pHead;
+    Node **current = &m_pHead;
+
+    while (otherCurent) {
+        *current = new Node(otherCurent->m_data, otherCurent->m_ref);
+        current = &((*current)->m_pNext);
+        otherCurent = otherCurent->m_pNext;
+    }
+}
+
 
 template <typename T>
 CLinkedList<T>::~CLinkedList()
 {
+    Clear();
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, CLinkedList<T>& list){
+    typename CLinkedList<T>::Node *current = list.m_pHead;
+    os <<"[";
+    while (current) {
+        os << "(" << current->GetData() << "," << current->GetRef() << ")";
+        if (current->GetNext()){
+            os << "->";
+        }
+        current = current->GetNext();
+    }
+    os << "]";
+    return os;
 }
 
 
